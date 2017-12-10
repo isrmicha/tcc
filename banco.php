b750490da7da282374d7921b581e95ff1b50b8bf
Israel <isrmicha@gmail.com>
2017-12-09 22:48:19 -0200

    e

--- banco.php
+++ banco.php
@@ -28,28 +28,74 @@ if ($result->num_rows > 0) {
 $conn->close();
 }
 
-if(isset($_GET['mostraTudo'])){
-    $conn = new mysqli($servername, $username, $password,$dbname);
-    if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);} 
-    $sql = "SELECT * FROM tccs";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $resultados = array();
-        while($row = $result->fetch_assoc()) {
-            $resultadoTemp = new Tcc();
-            $resultadoTemp->id = $row["id"];
-            $resultadoTemp->autor = $row["autor"];
-            $resultadoTemp->tema = $row["tema"];
-            $resultadoTemp->link = $row["link"];
-            $resultadoTemp->dataAtualizacao = $row["dataAtualizacao"];
-            array_push($resultados,$resultadoTemp);
+
+
+    if(isset($_GET['create'])){
+        $conn = new mysqli($servername, $username, $password,$dbname);
+        if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);} 
+        $autor = $_GET['autor'];
+        $tema = $_GET['tema'];
+        $link = $_GET['link'];
+        $sql = "INSERT INTO tccs (autor,tema,link) VALUES ('$autor','$tema','$link') ";
+        if ($conn->query($sql) === TRUE) {
+            echo "true";
+        } else {
+          echo "false";
         }
-        echo json_encode($resultados);
-    } else {
-       echo "0 resultados";
-    }
-    $conn->close();
-    }
+        $conn->close();
+        }
+
+        if(isset($_GET['read'])){
+            $conn = new mysqli($servername, $username, $password,$dbname);
+            if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);} 
+            $sql = "SELECT * FROM tccs";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                $resultados = array();
+                while($row = $result->fetch_assoc()) {
+                    $resultadoTemp = new Tcc();
+                    $resultadoTemp->id = $row["id"];
+                    $resultadoTemp->autor = $row["autor"];
+                    $resultadoTemp->tema = $row["tema"];
+                    $resultadoTemp->link = $row["link"];
+                    $resultadoTemp->dataAtualizacao = $row["dataAtualizacao"];
+                    array_push($resultados,$resultadoTemp);
+                }
+                echo json_encode($resultados);
+            } else {
+               echo "0 resultados";
+            }
+            $conn->close();
+            }
+
+        if(isset($_GET['update'])){
+            $conn = new mysqli($servername, $username, $password,$dbname);
+            if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);} 
+            $id = $_GET["id"];
+            $autor = $_GET['autor'];
+            $tema = $_GET['tema'];
+            $link = $_GET['link'];
+            $sql = "UPDATE tccs SET autor='$autor',tema ='$tema', link='$link' WHERE id='$id'";
+            if ($conn->query($sql) === TRUE) {
+                echo "true";
+            } else {
+              echo "false";
+            }
+            $conn->close();
+            }
+
+            if(isset($_GET['delete'])){
+                $conn = new mysqli($servername, $username, $password,$dbname);
+                if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);} 
+                $id = $_GET["id"];
+                $sql = "DELETE FROM tccs WHERE id='$id'";
+                if ($conn->query($sql) === TRUE) {
+                    echo "true";
+                } else {
+                  echo "false";
+                }
+                $conn->close();
+                }
 
     
    
